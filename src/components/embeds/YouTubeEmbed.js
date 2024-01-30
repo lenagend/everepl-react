a81da13c5b018a83e9d9ee0aebdbe8325d3daa03
lenagend <lenagend@naver.com>
2024-01-30 16:14:57 +0900

    유튜브 임베드 ui 수정

--- src/components/embeds/YouTubeEmbed.js
+++ src/components/embeds/YouTubeEmbed.js
@@ -15,8 +15,8 @@ export default function YouTubeEmbed({ url }){
         <Box sx={{p : 2,textAlign: 'center'}}>
             {videoId ? (
                 <iframe
-                    width="560"
-                    height="315"
+                    width="100%"
+                    height="350px"
                     src={`https://www.youtube.com/embed/${videoId}`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
