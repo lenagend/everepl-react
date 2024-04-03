bb51432412d64027aa616f82ff85380ac32ea923
lenagend <lenagend@naver.com>
2024-04-03 10:11:20 +0900

    feat(LoginCard): 3개사 소셜로그인기능 구현중
    새창으로 열어서 토큰 받아오는 로직 적용
    
    BREAKING CHANGE: 영향없음

--- src/components/login/LoginCard.js
+++ src/components/login/LoginCard.js
@@ -1,17 +1,30 @@
 import Card from "@mui/joy/Card";
-import CardContent from "@mui/joy/CardContent";
 import Stack from "@mui/joy/Stack";
-import CardOverflow from "@mui/joy/CardOverflow";
 import * as React from "react";
-import {Box, Divider, Typography} from "@mui/joy";
-import Link from "@mui/joy/Link";
-import LogoButton from "../iconButtons/LogoButton";
-import {CardHeader} from "@mui/material";
+
 
 export default function LoginCard() {
-    const googleLogin = () => {
-        window.location.href = `${process.env.REACT_APP_BACKEND_URL}/oauth2/authorization/google`; // 백엔드 OAuth2 로그인 경로로 리다이렉트
-    }
+    const handleLogin = (providerUrl) => {
+        const oauthWindow = window.open(
+            providerUrl,
+            'oauthPopup',
+            'width=600,height=700'
+        );
+
+        const handleOAuthMessage = (event) => {
+            if (event.origin !== "http://localhost:8080") return; // 메시지 소스 검증
+
+            const { token } = event.data;
+            if (token) {
+                oauthWindow.close();
+                window.removeEventListener('message', handleOAuthMessage);
+
+                // 토큰을 사용하여 애플리케이션 상태 업데이트
+            }
+        };
+
+        window.addEventListener('message', handleOAuthMessage, false);
+    };
 
     return (
         <Card
@@ -20,23 +33,29 @@ export default function LoginCard() {
             size="lg"
             variant="outlined"
         >
-        <Stack sx={{width: {
-                xs: '100%',
-                sm: 300,
-            }}}
-               spacing={1.5}
-               alignItems="center"
-        >
-            <Link href="http://localhost:8080/oauth2/authorization/google">
-                <img width="250" height={50} src="/images/sns/googleLoginButton.png" />
-            </Link>
-            <Link href="#variants" >
-                <img width="250" height={50} src="/images/sns/kakaoLoginButton.png" />
-            </Link>
-            <Link href="#variants">
-                <img width="250" height={50} src="/images/sns/naverLoginButton.png" />
-            </Link>
-        </Stack>
+            <Stack sx={{ width: { xs: '100%', sm: 300 } }} spacing={1.5} alignItems="center">
+                <img
+                    width="250"
+                    height={50}
+                    src="/images/sns/googleLoginButton.png"
+                    onClick={() => handleLogin('http://localhost:8080/oauth2/authorization/google')}
+                    style={{ cursor: 'pointer' }}
+                />
+                <img
+                    width="250"
+                    height={50}
+                    src="/images/sns/kakaoLoginButton.png"
+                    onClick={() => handleLogin('http://localhost:8080/oauth2/authorization/kakao')}
+                    style={{ cursor: 'pointer' }}
+                />
+                <img
+                    width="250"
+                    height={50}
+                    src="/images/sns/naverLoginButton.png"
+                    onClick={() => handleLogin('http://localhost:8080/oauth2/authorization/naver')}
+                    style={{ cursor: 'pointer' }}
+                />
+            </Stack>
         </Card>
-    )
+    );
 }
