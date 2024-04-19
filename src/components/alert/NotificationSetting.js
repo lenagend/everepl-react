94adc148ba8f3fb69e1c4264a86c45b4769878e8
lenagend <lenagend@naver.com>
2024-04-19 17:48:28 +0900

    feat(알림설정): 알림설정 기능 추가
    로그인한 유저의 알림설정 여부로 컴포넌트를 렌더링하고,
    스위치버튼으로 유저의 얼림설정 여부를 변경할 수 있게 패치

--- src/components/alert/NotificationSetting.js
+++ src/components/alert/NotificationSetting.js
@@ -2,14 +2,46 @@ import Typography from "@mui/joy/Typography";
 import {Switch} from "@mui/joy";
 import Stack from "@mui/joy/Stack";
 import * as React from "react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {useAuth} from "../../security/AuthProvider";
 
 export default function NotificationSetting(){
     const [checked, setChecked] = useState(true);
+    const { user, axiosInstance, useRequireAuth  } = useAuth();
+
+    const updateUser = () => {
+        if (!useRequireAuth) return;
+
+        const data = {
+            notificationSetting: checked
+        };
+
+        axiosInstance.patch(`http://localhost:8080/api/auth`, data)
+            .then(response => {
+                console.log(response.data);
+                setChecked(response.data.notificationSetting); // 서버 응답으로 상태 업데이트
+            })
+            .catch(error => {
+                console.error('Error fetching user:', error);
+            });
+    };
+
+    useEffect(() => {
+        if (user) {
+            setChecked(user.notificationSetting || false); // user가 존재하면 notificationSetting 값을 사용, 없으면 false
+        }
+    }, [user]);
 
     const handleNotificationSetting = (event) => {
         setChecked(event.target.checked)
+        updateUser();
     }
+
+    if (!user) {
+        return null;
+    }
+
     return(
         <Stack alignItems={"center"}>
             <Typography level={"title-sm"} sx={{color: 'white'}} component="label">
