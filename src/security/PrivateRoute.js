6155dcf9531fe3f07212335f12a9a12bc47ea5a0
lenagend <57205046+lenagend@users.noreply.github.com>
2024-05-16 11:09:44 +0900

    feat(privateRoutes): 로딩상태 변수 추가
    로딩상태 변수추가로 새로고침시에 권한변수가 로딩되기 전에 렌더링 시도하는것을 막음

--- src/security/PrivateRoute.js
+++ src/security/PrivateRoute.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import {useAuth} from "./AuthProvider";
+import Box from "@mui/joy/Box";
+
 
 const PrivateRoute = ({ element: Component }) => {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, isAuthLoading } = useAuth();
     const location = useLocation();
+
+    if (isAuthLoading) {
+        return <Box>권한확인중입니다...</Box>; // 권한 체크 로딩 중일 때 표시할 컴포넌트
+    }
 
     return isAuthenticated ? <Component /> : <Navigate to="/login" state={{ from: location.pathname }} />;
 };
-
 
 export default PrivateRoute;
